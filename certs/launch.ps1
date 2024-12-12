6fcb0f9dca6f5fe1e8227c0ae552886a35a05cc6
mykquayce <myk_quayce@hotmail.com>
2024-12-12 00:26:06 +0000

    certificates (#2)
    * work so far
    
    * homepage
    
    * external services
    
    * ahti and pope
    
    * fixing speedtest
    
    * fixing cert storage
    
    ---------
    
    Co-authored-by: bob h <mail@bob.codes>

--- certs/launch.ps1
+++
@@ -1,8 +0,0 @@
-docker pull alpine:latest
-if (!$?) { exit; }
-
-docker run --interactive=true --rm --tty=true `
-	--volume "${env:userprofile}\.aspnet\https\:/opt/certs/:rw" `
-	--volume '.:/root/certs/:rw' `
-	alpine:latest `
-	sh -c 'cd /root/certs/; dos2unix *.*; exec /bin/sh'
