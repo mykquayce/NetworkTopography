4923d82e4042a9d8f020d606a0ec116f75fe6abe
bob h <mail@bob.codes>
2026-02-25 19:34:09 +0000

    changedetection (#29)
    * checking authentik's docker-compose.yml
    
    * re-working ChangeDetection watches script
    
    ...to include tags
    
    ---------
    
    Co-authored-by: bob h <mail@bob.codes>

--- watches.ps1
+++ watches.ps1
@@ -1,36 +1,59 @@
-$watches = `
-	#apps
-	'{"url":"https://docs.goauthentik.io/docker-compose.yml"}', `
-	'{"title":"https://gethomepage.dev/widgets/","url":"https://github.com/gethomepage/homepage/commits/dev/docs/widgets.atom"}', `
-	'{"title":"https://github.com/karakeep-app/karakeep/blob/main/docker/docker-compose.yml","url":"https://raw.githubusercontent.com/karakeep-app/karakeep/refs/heads/main/docker/docker-compose.yml"}', `
-	'{"title":"https://github.com/paperless-ngx/paperless-ngx/blob/dev/docker/compose/docker-compose.postgres.yml","url":"https://raw.githubusercontent.com/paperless-ngx/paperless-ngx/refs/heads/dev/docker/compose/docker-compose.postgres.yml"}', `
-	'{"title":"https://github.com/plankanban/planka/blob/master/docker-compose.yml","url":"https://raw.githubusercontent.com/plankanban/planka/refs/heads/master/docker-compose.yml"}', `
-	'{"title":"https://github.com/go-vikunja/website/blob/main/src/content/docs/setup/docker-start-to-finish.mdoc","url":"https://raw.githubusercontent.com/go-vikunja/website/refs/heads/main/src/content/docs/setup/docker-start-to-finish.mdoc"}', `
-
-	#hardware
-	'{"include_filters":["//div[contains(@class, ''cartridge-search-results'')]"],"url":"https://evercade.co.uk/cartridges/"}', `
-	'{"include_filters":["//*[contains(@class, ''devicename'')]"],"url":"https://wiki.lineageos.org/devices/"}', `
-
-	#internet
-	'{"include_filters":["json:$.ip"],"url":"https://api.ipapi.is"}', `
-	'{"url":"https://www.internic.net/domain/named.cache"}', `
-
-	#kiwix
-	'{"extract_text":["/gutenberg_en_all_.+?\\.zim/"],"url":"https://download.kiwix.org/zim/gutenberg/"}', `
-	'{"extract_text":["/wikipedia_en_all_maxi_.+?\\.zim/"],"url":"https://download.kiwix.org/zim/wikipedia/"}', `
-	'{"extract_text":["/wikiquote_en_all_maxi_.+?\\.zim/"],"url":"https://download.kiwix.org/zim/wikiquote/"}', `
-	'{"extract_text":["/wiktionary_en_all_.+?\\.zim/"],"url":"https://download.kiwix.org/zim/wiktionary/"}', `
-
-	#software
-	'{"title":"https://github.com/awesome-selfhosted/awesome-selfhosted","url":"https://raw.githubusercontent.com/awesome-selfhosted/awesome-selfhosted/refs/heads/master/README.md"}', `
-	'{"url":"https://docs.docker.com/desktop/release-notes/"}', `
-	'{"include_filters":["#supported-versions-table"],"url":"https://dotnet.microsoft.com/en-us/download/dotnet"}', `
-	'{"include_filters":["json:$.IDS[0].downloadInfo.DownloadURL"],"title":"Nvidia drivers","url":"https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=120&pfid=942&osID=57&languageCode=1078&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&qnf=0&ctk=null&sort1=1&numberOfResults=1"}', `
-	'{"url":"https://ubuntu.com/download/server"}'
-
-foreach ($watch in $watches) {
-	curl --data "$watch" `
-		--header "Content-Type: application/json" `
-		--header "x-api-key: ${env:ChangeDetectionApiKey}" `
-		--url 'https://changedetection.bob.house/api/v1/watch'
+function Write-Log([string] $Message) {
+	$Now = Get-Date -Format "HH:mm:ss"
+	Write-Output "$Now : $Message"
 }
+
+function Add-Tag([string] $Title) {
+	$Body = @{'title' = $Title}
+	$Response = Invoke-WebRequest `
+		-Body ($Body | ConvertTo-Json -Compress) `
+		-Headers @{'Content-Type' = 'application/json'; 'x-api-key' = "${env:ChangeDetectionApiKey}"} `
+		-Method POST `
+		-Uri 'https://changedetection.bob.house/api/v1/tag'
+	$TagId = ($Response.Content | jq --raw-output .uuid)
+	return $TagId
+}
+
+function Add-Watch([string] $ExtractText, [string] $IncludeFilters, [string] $Tag, [string] $Title, [string] $Uri) {
+	$TagId = (Add-Tag -Title "$Tag")
+
+	$Body = @{}
+	if ($ExtractText) { $Body.extract_text = @($ExtractText) }
+	if ($IncludeFilters) { $Body.include_filters = @($IncludeFilters) }
+	if ($Tag) { $Body.tags = @($TagId) }
+	if ($Title) { $Body.title = $Title }
+	if ($Uri) { $Body.url = $Uri }
+
+	$Response = Invoke-WebRequest `
+		-Body ($Body | ConvertTo-Json -Compress) `
+		-Headers @{'Content-Type' = 'application/json'; 'x-api-key' = "${env:ChangeDetectionApiKey}"} `
+		-Method POST `
+		-Uri 'https://changedetection.bob.house/api/v1/watch'
+
+	Write-Log "Added: $Uri"
+}
+
+
+Add-Watch -Tag 'apps' -Title 'https://github.com/goauthentik/authentik/blob/main/.github/actions/setup/compose.yml' -Uri 'https://raw.githubusercontent.com/goauthentik/authentik/refs/heads/main/.github/actions/setup/compose.yml'
+Add-Watch -Tag 'apps' -Title 'https://gethomepage.dev/widgets/' -Uri 'https://github.com/gethomepage/homepage/commits/dev/docs/widgets.atom'
+Add-Watch -Tag 'apps' -Title 'https://github.com/karakeep-app/karakeep/blob/main/docker/docker-compose.yml' -Uri 'https://raw.githubusercontent.com/karakeep-app/karakeep/refs/heads/main/docker/docker-compose.yml'
+Add-Watch -Tag 'apps' -Title 'https://github.com/paperless-ngx/paperless-ngx/blob/dev/docker/compose/docker-compose.postgres.yml' -Uri 'https://raw.githubusercontent.com/paperless-ngx/paperless-ngx/refs/heads/dev/docker/compose/docker-compose.postgres.yml'
+Add-Watch -Tag 'apps' -Title 'https://github.com/plankanban/planka/blob/master/docker-compose.yml' -Uri 'https://raw.githubusercontent.com/plankanban/planka/refs/heads/master/docker-compose.yml'
+Add-Watch -Tag 'apps' -Title 'https://github.com/go-vikunja/website/blob/main/src/content/docs/setup/docker-start-to-finish.mdoc' -Uri 'https://raw.githubusercontent.com/go-vikunja/website/refs/heads/main/src/content/docs/setup/docker-start-to-finish.mdoc'
+
+Add-Watch -Tag 'hardware' -IncludeFilters '//div[contains(@class, "cartridge-search-results")]' -Uri 'https://evercade.co.uk/cartridges/'
+Add-Watch -Tag 'hardware' -IncludeFilters '//*[contains(@class, "devicename")]' -Uri 'https://wiki.lineageos.org/devices/'
+
+Add-Watch -Tag 'internet' -IncludeFilters 'json:$.ip' -Uri 'https://api.ipapi.is'
+Add-Watch -Tag 'internet' -Uri 'https://www.internic.net/domain/named.cache'
+
+Add-Watch -Tag 'kiwix' -ExtractText '/gutenberg_en_all_.+?\.zim/' -Uri 'https://download.kiwix.org/zim/gutenberg/'
+Add-Watch -Tag 'kiwix' -ExtractText '/wikipedia_en_all_maxi_.+?\.zim/' -Uri 'https://download.kiwix.org/zim/wikipedia/'
+Add-Watch -Tag 'kiwix' -ExtractText '/wikiquote_en_all_maxi_.+?\.zim/' -Uri 'https://download.kiwix.org/zim/wikiquote/'
+Add-Watch -Tag 'kiwix' -ExtractText '/wiktionary_en_all_.+?\.zim/' -Uri 'https://download.kiwix.org/zim/wiktionary/'
+
+Add-Watch -Tag 'software' -title 'https://github.com/awesome-selfhosted/awesome-selfhosted' -Uri 'https://raw.githubusercontent.com/awesome-selfhosted/awesome-selfhosted/refs/heads/master/README.md'
+Add-Watch -Tag 'software' -Uri 'https://docs.docker.com/desktop/release-notes/'
+Add-Watch -Tag 'software' -IncludeFilters '#supported-versions-table' -Uri 'https://dotnet.microsoft.com/en-us/download/dotnet'
+Add-Watch -Tag 'software' -IncludeFilters 'json:$.IDS[0].downloadInfo.DownloadURL' -Title 'Nvidia drivers' -Uri 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=120&pfid=942&osID=57&languageCode=1078&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&qnf=0&ctk=null&sort1=1&numberOfResults=1'
+Add-Watch -Tag 'software' -Uri 'https://ubuntu.com/download/server'
