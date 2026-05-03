602d9002d4dcaa24652fc54d727eb264d9212e4a
bob h <mail@bob.codes>
2026-05-03 20:18:43 +0100

    tidying directory mappings
    changing log and cache directories to temp directories in the containers; making in-line .kopia-ignore files; merging setup scripts

--- billups/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-function link() {
-	ln --force --no-target-directory --verbose $1 $2
-}
-
-printf '/cache/*\n/logs/*\n' > ~/.kopia/.kopiaignore
-
-link  ./netplan.yaml  /etc/netplan/50-cloud-init.yaml
